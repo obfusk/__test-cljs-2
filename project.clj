d6a01002eb53810520a4d27f2e54485a7b5aebf6
Felix C. Stegerman <flx@obfusk.net>
2012-03-24 23:13:07 +0100

    initial commit; + .gitignore, README; + project.clj, src-cljs/test1/core.clj
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject cljs-test-1 "1.0.0-SNAPSHOT"
+  :description        "..."
+  :source-path        "src-clj"
+  :dependencies       [[ org.clojure/clojure  "1.3.0" ]]
+  :plugins            [[ lein-cljsbuild       "0.1.3" ]]
+  :cljsbuild { :builds [{
+    :source-path      "src-cljs"
+    :compiler {
+      :output-to      "js/test1.js"
+      :optimizations  :whitespace
+      :pretty-print   true
+  } }] } )
