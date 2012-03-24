d5e1a977cad49d1b7e62d93d37b4040694a81d13
Felix C. Stegerman <flx@obfusk.net>
2012-03-24 23:47:19 +0100

    ~ project.clj (jayq version; + crate)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject cljs-test-1 "1.0.0-SNAPSHOT"
   :description        "..."
   :source-path        "src-clj"
-  :dependencies       [[ org.clojure/clojure  "1.3.0"         ]
-                       [ jayq                 "0.1.0-alpha2"  ]]
-  :plugins            [[ lein-cljsbuild       "0.1.3"         ]]
+  :dependencies       [ [ org.clojure/clojure "1.3.0"           ]
+                        [ jayq                "0.1.0-alpha3"    ]
+                        [ crate               "0.2.0-snapshot"  ] ]
+  :plugins            [ [ lein-cljsbuild      "0.1.3"           ] ]
   :cljsbuild { :builds [{
     :source-path      "src-cljs"
     :compiler {
