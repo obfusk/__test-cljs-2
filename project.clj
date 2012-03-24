6fd81ef868e40e0caac7de2992d7c77b3cec8090
Felix C. Stegerman <flx@obfusk.net>
2012-03-24 23:38:16 +0100

    ~ project.clj (+ jayq)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject cljs-test-1 "1.0.0-SNAPSHOT"
   :description        "..."
   :source-path        "src-clj"
-  :dependencies       [[ org.clojure/clojure  "1.3.0" ]]
-  :plugins            [[ lein-cljsbuild       "0.1.3" ]]
+  :dependencies       [[ org.clojure/clojure  "1.3.0"         ]
+                       [ jayq                 "0.1.0-alpha2"  ]]
+  :plugins            [[ lein-cljsbuild       "0.1.3"         ]]
   :cljsbuild { :builds [{
     :source-path      "src-cljs"
     :compiler {
